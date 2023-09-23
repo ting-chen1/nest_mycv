b2af6b883e706790dc5602345c28f322b91caa98
ting-chen1 <ting.chen@nextlink.com.tw>
2023-09-23 14:00:23 +0800

    以 decorator 方式使用 interceptor   1. 用 DI 實作，根據接收到的 DTO 決定資料處理格式   2. 定義 decorator 參數型別，必須是 class   3. 套用 interceptor 到 route 或整個 controller

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -1,6 +1,7 @@
-import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { CallHandler, ExecutionContext, NestInterceptor, UseInterceptors } from '@nestjs/common';
 import { map, Observable } from 'rxjs';
-import { plainToClass } from 'class-transformer';
+import { plainToInstance } from 'class-transformer';
+// import { UserDto } from '../users/dto/user.dto'; // 範例暫時寫死 User DTO，後續會改用 constructor 處理
 
 // interceptor 可用於 route, controller, global 層級
 // 處理 request 與 reponse
@@ -16,6 +17,21 @@ import { plainToClass } from 'class-transformer';
 //   在此可以視為後續 route 會執行的 function
 // }
 
+
+// 這裡的型別寫 any 時，使用區塊如果傳入 DTO 以外的資訊
+// typescript 不會顯示錯誤，但實際運行會噴錯
+// 至少要讓 dto 是 class 而非任意資訊
+// export function Serialize(dto: any) {
+//   return UseInterceptors(new SerializeInterceptor(dto));
+// }
+
+// 此 interface 代表的是任何 class
+interface ClassConstructor {
+  new (...args: any[]): {}
+}
+export function Serialize(dto: ClassConstructor) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
 export class SerializeInterceptor implements NestInterceptor {
   // 可以用 implements class 或是 implements interface
   // implements 是將後方的 class 或 interface 內定義的方法或屬性套用到目標 class
@@ -23,14 +39,30 @@ export class SerializeInterceptor implements NestInterceptor {
   // 以此舉例， typescript 會檢查 SerializeInterceptor 內是否滿足 NestInterceptor 的設定
   // 在此會檢查 SerializeInterceptor 內是否定義 intercept 這個方法
 
+  constructor(private dto: any) {}
+
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
     // request 被 controller 處理前，這裡的程式會先執行
-    console.log('I am running before the handler', context);
+    // console.log('I am running before the handler', context);
 
     return handler.handle().pipe(
       map((data: any) => {
         // response 傳出之前，這裡的程式會先執行
-        console.log('I am running before response is sent out', data);
+        // console.log('I am running before response is sent out', data);
+
+        // plainToInstance 是將一般的 object 轉變成某個類別的實體
+        // data 指的是 response 要送出得資訊，在此攔截下來做調整
+        // excludeExtraneousValues: true
+        // 表示除了標記在 DTO 裡要顯示的資訊，其他都要去除
+        // 這樣是寫死 UserDto ，需要改成根據傳入的 DTO 做清洗
+        // 這會需要寫 constructor 保留傳入的 DTO
+        // return plainToInstance(UserDto, data, {
+        //   excludeExtraneousValues: true,
+        // })
+
+        return plainToInstance(this.dto, data, {
+          excludeExtraneousValues: true,
+        })
       }),
     );
   }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -14,9 +14,13 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { SerializeInterceptor } from '../interceptors/serialize.interceptor';
+// import { SerializeInterceptor } from '../interceptors/serialize.interceptor';
+// 後續包成 decorator，可直接使用 decorator
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { UserDto } from './dto/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto) // 這樣是將此 interceptor 套用到整個 controller 上
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
@@ -38,7 +42,26 @@ export class UsersController {
   // 這是使用客製化的 interceptor
   // 可以避免預設 interceptor 使用 entity 設定 exclude 造成全域影響的問題
   // 在客製化的 interceptor 內設定該 route 需要處理的資料即可
-  @UseInterceptors(SerializeInterceptor)
+  // @UseInterceptors(SerializeInterceptor) 當 interceptor 寫死的時候可以這樣使用
+  // @Get(':id')
+  // async findUser(@Param('id') id: string) {
+  //    ........
+  //    ........
+  // }
+
+  // 將 Dto 當參數傳給 interceptor 可以重複利用 interceptor，並做到 dependency injection
+  // 這樣的寫法頗長，後續改寫成客製化 decorator，方便使用
+  // @UseInterceptors(new SerializeInterceptor(UserDto))
+  // @Get(':id')
+  // async findUser(@Param('id') id: string) {
+  //    ........
+  //    ........
+  // }
+
+  // 最終 interceptor 以 decorator 方式使用
+  // Serialize 根據 DTO 決定資料清洗的情況
+  // Serialize 可以用在單一 route 也可以用在整個 controller
+  // @Serialize(UserDto)
   @Get(':id')
   async findUser(@Param('id') id: string) {
     // request 傳進來時，url 整體是一個字串，nest 不會自動解析成數字
