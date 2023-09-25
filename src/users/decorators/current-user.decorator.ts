1a8073e5273afd6aa59581a87fa14e13058c5950
ting-chen1 <ting.chen@nextlink.com.tw>
2023-09-25 17:53:46 +0800

    補上 decorator 不在與 DI 系統內的註解  所以 CurrentUser 無法直接使用 UsersService 的方法  要透過 interceptor

--- src/users/decorators/current-user.decorator.ts
+++ src/users/decorators/current-user.decorator.ts
@@ -3,6 +3,9 @@ import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 // 使用 decorator 取得 session 資訊
 // 但 decorator 並不能在 DI 系統中取用到 user repository，但 interceptor 可以
 // 所以需要再做一個 interceptor 處理
+// 課程原話
+// Param decorators exist outside the DI system, so
+// our decorator can't get an instance of UsersService directly.
 export const CurrentUser = createParamDecorator(
   // data 是指任何傳入 decorator 的資訊
   // 對於 currentUser 來說，不希望傳入任何參數，所以型別設定成 never
