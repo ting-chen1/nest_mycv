d39cc3a491ebc7349895d51060b6a331ebb0c541
ting-chen1 <ting.chen@nextlink.com.tw>
2023-09-25 10:26:46 +0800

    install cookie-session package

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
+const cookieSession = require('cookie-session')
+// cookie-session 與 typescript 有部分相容性問題
+// 用 require 引入可以解決
+
 // whitelist: true 的功能是去除未定義的參數
 // 類似 rails params.permit(xxxxx)
 // 只保留該路徑定義的 DTO 內參數
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieSession({
+    keys: ['asdfasdf']
+  }))
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
 }
