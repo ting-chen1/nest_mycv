1654ecd4c016d070184d412f92dfd52c51e54c3e
ting-chen1 <ting.chen@nextlink.com.tw>
2023-10-02 17:52:27 +0800

    reports create 包含 controller service dto entity

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { CreateReportDto } from './dto/create-report-dto';
+import { ReportsService } from './reports.service';
+import { AuthGuard } from '../guard/auth.guard';
+
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  constructor(private reportService: ReportsService) {}
+
+  @Post()
+  @UseGuards(AuthGuard)
+  createReport(@Body() body: CreateReportDto) {
+    return this.reportService.create(body);
+  }
+}
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Report } from './reports.entity';
+import { CreateReportDto } from './dto/create-report-dto';
+
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  // InjectRepository 則是告知 DI 系統，這裡會用到 Report repository
+  // 這個修飾器只是因為 DI 系統與 通用型別相容不夠好，所以才需要使用
+  constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+
+  create(reportDto: CreateReportDto) {
+    const report = this.repo.create(reportDto)
+    return this.repo.save(report)
+  }
+}
